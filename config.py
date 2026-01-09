8471401a3a988baa6d2ce3a8313dea733694b5cf
Seamus Kane <kane.sea@northeastern.edu>
2026-01-08 20:03:24 -0500

    first phase

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+Docstring for config
+"""
+
+MUSIC_FOLDER = "/path/to/your/music"  # Change this
+EXCERPT_LENGTH = 5.0  # seconds
+SUPPORTED_FORMATS = {".mp3", ".wav", ".flac", ".ogg", ".m4a"}
+CACHE_FILE = "onset_cache.json"
